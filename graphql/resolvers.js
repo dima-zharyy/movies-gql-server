2a6acdaa23ccd3fe10a537d05cc2a507a1ef02af
dima-zharyy <zharyydimka@gmail.com>
2022-10-03 10:33:08 +0300

    added typeDefs and resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,17 @@
+import {
+  getCredits,
+  getDetails,
+  getMovies,
+  getReviews,
+  getTrending,
+} from '../services/movies-api.js';
+
 export const resolvers = {
   Query: {
-    name: () => 'Dima Zharyy',
+    movies: () => getTrending(),
+    searchMovies: (_, { query }) => getMovies(query),
+    details: (_, { id }) => getDetails(id),
+    credits: (_, { id }) => getCredits(id),
+    reviews: (_, { id }) => getReviews(id),
   },
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,5 +1,45 @@
+// You can add all fields from third party API and use in queries only necessary fields
+
 export const typeDefs = `
+type Movie {
+  id: Int!
+  poster_path: String!
+  title: String!
+}
+
+type Genre {
+  id: Int!
+  name: String!
+}
+
+type Details {
+  title: String!
+  overview: String!
+  vote_average: Float!
+  genres: [Genre]!
+  poster_path: String!
+}
+
+type Credits {
+  id: Int!
+  profile_path: String!
+  name: String!
+  character: String!
+}
+
+type Reviews {
+  id: Int!
+  author: String!
+  updated_at: String!
+  content: String!
+}
+
 type Query {
-  name: String!,
-  }
+  movies: [Movie]!
+  searchMovies(query: String!): [Movie]!
+  details(id: Int!): Details!
+  credits(id: Int!): [Credits]!
+  reviews(id: Int!): [Reviews]!
+  
+}
 `;
